671b8f4e3cb2b7b8f2180f5512fbf4d38f33a120
Fadi Mashan <fadi.mashan@gmail.com>
2021-03-09 14:11:11 +0100

    init model and controller for the products

---
+++ Grupp5/Data/Grupp5Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Grupp5.Models;
+
+namespace Grupp5.Data
+{
+    public class Grupp5Context : DbContext
+    {
+        public Grupp5Context (DbContextOptions<Grupp5Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Grupp5.Models.Product> Product { get; set; }
+    }
+}
