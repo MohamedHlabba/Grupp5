7744420936c6241665c5875ebdd35af4bf1a47ac
Fadi Mashan <fadi.mashan@gmail.com>
2021-03-09 14:22:17 +0100

    edite model

--- Grupp5/Models/Product.cs
+++ Grupp5/Models/Product.cs
@@ -10,9 +10,7 @@ namespace Grupp5.Models
 
         public int Id  { get; set; }
 
-        public string Name { get; set; }
-
-        public int Price { get; set; }
+        public int Name { get; set; }
 
 
     }
